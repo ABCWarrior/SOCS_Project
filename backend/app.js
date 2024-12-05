8e811f9a47ff01b654b32a6c944699a962857659
yoman321 <luophilipe@gmail.com>
2024-12-05 18:57:56 -0500

    added instructions to get started

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+require('dotenv').config({ path: '../.env' });
+
+const express = require('express')
+const app = express();
+const port = process.env.BACKEND_HOST;
+
+app.get('/', (req, res) => {
+  res.send(('Hello World!'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- backend/readme.md
+++ backend/readme.md
@@ -1,1 +1,11 @@
 # SOCS Project backend in Express.js 
+
+## To run
+```
+npm install
+node app.js
+```
+
+## ToDo
+
+## Issues
