b3982e6378cf30be1daf290ce8896662cff3cb47
Aakarsh Dhar <dharaakarsh@gmail.com>
2024-12-17 21:23:40 -0500

    Fixed collection issue

--- backend/services/guestsService.js
+++ backend/services/guestsService.js
@@ -1,7 +1,6 @@
 import database from '../database/connectDatabase.js';
 import { bookingsEnums } from '../enums/bookingsEnums.js';
-import { membersCollection } from '../database/connectDatabase.js';
-
+const membersCollection = database.collection(process.env.MONGO_MEMBERS_COLLECTION);
 const bookingsCollection = database.collection(process.env.MONGO_BOOKINGS_COLLECTION);
 
 export const getGuestAttendance = async (email) => {
