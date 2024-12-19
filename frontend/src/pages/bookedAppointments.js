00398745fed9e4c8b34482a1f6f1644ac35cbeed
Celia Shi <celiashi666@hotmail.com>
2024-12-19 16:08:39 -0500

    finals changes, tried debugging myRequests and added bookedAppointments calls

--- frontend/src/pages/bookedAppointments.js
+++ frontend/src/pages/bookedAppointments.js
@@ -8,21 +8,51 @@ function Booked() {
   const [search, setSearch] = useState("");
   const [bookings, setBookings] = useState([]);
 
-  const userEmail = localStorage.getItem('guestEmail');
-
   const filteredBookings = () => {
     return bookings.filter((booking) =>
       booking.professor.toLowerCase().includes(search.toLowerCase())
     )
   }
 
+  //wrote 2 api one for guests which is the commented version and one for members, i'll let you guys do the 
+  //logic of knowing which one to call
+
+  // const userEmail = localStorage.getItem('guestEmail');
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       await fetch(`http://127.0.0.1:5000/api/guests/`, {
+  //         method: "GET",
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           'email': userEmail
+  //         }
+  //       })
+  //         .then(res => res.json())
+  //         .then(data => {
+  //           setBookings(data.all_bookings || []);
+  //         })
+  //     }
+  //     catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+  //   fetchData()
+  // }, []);
+
+  const userEmail = localStorage.getItem('guestEmail');
+  const id = localStorage.getItem("userId");
+  const token = localStorage.getItem('token');
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await fetch('http://127.0.0.1:5000/api/guests/', {
+        await fetch(`http://127.0.0.1:5000/api/members/${id}/request_attendance`, {
           method: "GET",
           headers: {
             'Content-Type': 'application/json',
+            'token': token,
             'email': userEmail
           }
         })
@@ -62,9 +92,12 @@ function Booked() {
                 startTime={booking.startTime}
                 endTime={booking.endTime}
                 isRecurring={booking.isRecurring}
+                //these calendar events should have no buttons on the side, in my calendar implementation
+                //the last switch statement i return null for buttons so yeah should work if the page is an empty string
+                //else just make anoter swtich statement ig
                 page=""
-                bookingId=""
-                email=""
+                bookingId="" //might need it idk, but since its just view i dont think so
+                email=""//same here
               />
             ))}
           </div>
