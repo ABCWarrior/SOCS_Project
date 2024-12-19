e583e95a3e8ee7708ad5203140cee55ccfe4cc55
yoman321 <luophilipe@gmail.com>
2024-12-19 09:33:24 -0500

    formaatted code and making it ready for review

--- frontend/src/pages/Request.js
+++ frontend/src/pages/Request.js
@@ -4,209 +4,205 @@ import Footer from '../components/Footer.js';
 import "../styles/Request.css";
 
 const Request = () => {
-    const [formData, setFormData] = useState({
-        date: "",
-        month: "",
-        fromHour: "",
-        fromMin: "",
-        toHour: "",
-        toMin: "",
-    });
+  const [formData, setFormData] = useState({
+    date: "",
+    month: "",
+    fromHour: "",
+    fromMin: "",
+    toHour: "",
+    toMin: "",
+  });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const isFormValid = Object.values(formData).every((value) => value !== "");
+    const isFormValid = Object.values(formData).every((value) => value !== "");
 
-        if (!isFormValid) {
-            alert("Please fill out all the required fields before submitting.");
-            return;
-        }
+    if (!isFormValid) {
+      alert("Please fill out all the required fields before submitting.");
+      return;
+    }
 
-        const year = new Date().getFullYear();
-        const monthNames = [
-            "January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"
-        ];
-        const monthIndex = monthNames.indexOf(formData.month) + 1;
-        const month = monthIndex.toString().padStart(2, "0");
-    
-        const day = formData.date.toString().padStart(2, "0");
-        const date = `${year}-${month}-${day}`;
+    const year = new Date().getFullYear();
+    const monthNames = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    const monthIndex = monthNames.indexOf(formData.month) + 1;
+    const month = monthIndex.toString().padStart(2, "0");
 
-        var startTime = `${formData.fromHour.padStart(2, "0")}:${formData.fromMin.padStart(2, "0")}`;
-        var endTime = `${formData.toHour.padStart(2, "0")}:${formData.toMin.padStart(2, "0")}`;
-        const professor = localStorage.getItem('requestProf');
-        const bookingId = localStorage.getItem('requestBookingId');
-        const userEmail = localStorage.getItem('guestEmail');
-        // const professor = "Matthew";
-        // const bookingId = "6763ce6b94edf13c1898c952";
-        // const userEmail = "Matthew@mail.mcgill.ca";
-        // console.log(`http://localhost:5000/api/bookings/${bookingId}/edit_booking`) test
+    const day = formData.date.toString().padStart(2, "0");
+    const date = `${year}-${month}-${day}`;
 
-        try {
-            const response = await fetch(`http://localhost:5000/api/bookings/${bookingId}/appointment_request`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    userEmail, 
-                    professor, 
-                    date, 
-                    startTime, 
-                    endTime
-                }),
-            });
+    var startTime = `${formData.fromHour.padStart(2, "0")}:${formData.fromMin.padStart(2, "0")}`;
+    var endTime = `${formData.toHour.padStart(2, "0")}:${formData.toMin.padStart(2, "0")}`;
+    const professor = localStorage.getItem('requestProf');
+    const bookingId = localStorage.getItem('requestBookingId');
+    const userEmail = localStorage.getItem('guestEmail');
 
-            const result = await response.json();
+    try {
+      const response = await fetch(`http://localhost:5000/api/bookings/${bookingId}/appointment_request`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userEmail,
+          professor,
+          date,
+          startTime,
+          endTime
+        }),
+      });
 
-            if (response.ok) {
-                console.log("Booking request successfully:", result.message);
-            } else {
-                console.error("Failed to request booking:", result.message);
-            }
-        } catch (err) {
-            console.error("Error during API call:", err);
-        }
+      const result = await response.json();
 
-        console.log("Form Data Submitted:", 
-            userEmail, 
-            professor, 
-            date, 
-            startTime, 
-            endTime
-        );
-    };
+      if (response.ok) {
+        console.log("Booking request successfully:", result.message);
+      } else {
+        console.error("Failed to request booking:", result.message);
+      }
+    } catch (err) {
+      console.error("Error during API call:", err);
+    }
+
+    console.log("Form Data Submitted:",
+      userEmail,
+      professor,
+      date,
+      startTime,
+      endTime
+    );
+  };
 
   return (
     <div className="container">
-		<Header/>
-        <div className="request-booking-container">
-            <div className="request-booking-content">
-                <h1>Reschedule your appointment</h1>
-                <form onSubmit={handleSubmit} className="request-booking-form">
-                    <div className="request-date-time-container">
-                        <>
-                            <select
-                                name="date"
-                                value={formData.date}
-                                onChange={handleChange}
-                                className="dropdown"
-                            >
-                                <option value="" disabled>Date</option>
-                                {[...Array(31)].map((_, i) => (
-                                <option key={i} value={i + 1}>
-                                    {i + 1}
-                                </option>
-                                ))}
-                            </select>
+      <Header />
+      <div className="request-booking-container">
+        <div className="request-booking-content">
+          <h1>Reschedule your appointment</h1>
+          <form onSubmit={handleSubmit} className="request-booking-form">
+            <div className="request-date-time-container">
+              <>
+                <select
+                  name="date"
+                  value={formData.date}
+                  onChange={handleChange}
+                  className="dropdown"
+                >
+                  <option value="" disabled>Date</option>
+                  {[...Array(31)].map((_, i) => (
+                    <option key={i} value={i + 1}>
+                      {i + 1}
+                    </option>
+                  ))}
+                </select>
 
-                            <select
-                            name="month"
-                            value={formData.month}
-                            onChange={handleChange}
-                            className="dropdown"
-                            >
-                                <option value="" disabled>Month</option>
-                                {[
-                                    "January",
-                                    "February",
-                                    "March",
-                                    "April",
-                                    "May",
-                                    "June",
-                                    "July",
-                                    "August",
-                                    "September",
-                                    "October",
-                                    "November",
-                                    "December",
-                                ].map((month, i) => (
-                                    <option key={i} value={month}>
-                                        {month}
-                                    </option>
-                                ))}
-                            </select>
-                        </>
-                    </div>
-
-                    <div className="request-time-container">
-    <div className="request-time-row">
-        <label>From:</label>
-        <select
-            name="fromHour"
-            value={formData.fromHour}
-            onChange={handleChange}
-            className="dropdown request-time-dropdown"
-        >
-            <option value="" disabled>Hour</option>
-            {[...Array(24)].map((_, i) => (
-                <option key={i} value={i}>
-                    {i.toString().padStart(2, "0")}
-                </option>
-            ))}
-        </select>
-
-        <select
-            name="fromMin"
-            value={formData.fromMin}
-            onChange={handleChange}
-            className="dropdown request-time-dropdown"
-        >
-            <option value="" disabled>Min</option>
-            {[0, 15, 30, 45].map((min) => (
-                <option key={min} value={min}>
-                    {min.toString().padStart(2, "0")}
-                </option>
-            ))}
-        </select>
-    </div>
-
-    <div className="time-row">
-        <label>To:</label>
-        <select
-            name="toHour"
-            value={formData.toHour}
-            onChange={handleChange}
-            className="dropdown request-time-dropdown"
-        >
-            <option value="" disabled>Hour</option>
-            {[...Array(24)].map((_, i) => (
-                <option key={i} value={i}>
-                    {i.toString().padStart(2, "0")}
-                </option>
-            ))}
-        </select>
-
-        <select
-            name="toMin"
-            value={formData.toMin}
-            onChange={handleChange}
-            className="dropdown request-time-dropdown"
-        >
-            <option value="" disabled>Min</option>
-            {[0, 15, 30, 45].map((min) => (
-                <option key={min} value={min}>
-                    {min.toString().padStart(2, "0")}
-                </option>
-            ))}
-        </select>
-    </div>
-</div>
-
-                    <button type="submit" className="submit-button">
-                        Request
-                    </button>
-                </form>
+                <select
+                  name="month"
+                  value={formData.month}
+                  onChange={handleChange}
+                  className="dropdown"
+                >
+                  <option value="" disabled>Month</option>
+                  {[
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                    "July",
+                    "August",
+                    "September",
+                    "October",
+                    "November",
+                    "December",
+                  ].map((month, i) => (
+                    <option key={i} value={month}>
+                      {month}
+                    </option>
+                  ))}
+                </select>
+              </>
             </div>
+
+            <div className="request-time-container">
+              <div className="request-time-row">
+                <label>From:</label>
+                <select
+                  name="fromHour"
+                  value={formData.fromHour}
+                  onChange={handleChange}
+                  className="dropdown request-time-dropdown"
+                >
+                  <option value="" disabled>Hour</option>
+                  {[...Array(24)].map((_, i) => (
+                    <option key={i} value={i}>
+                      {i.toString().padStart(2, "0")}
+                    </option>
+                  ))}
+                </select>
+
+                <select
+                  name="fromMin"
+                  value={formData.fromMin}
+                  onChange={handleChange}
+                  className="dropdown request-time-dropdown"
+                >
+                  <option value="" disabled>Min</option>
+                  {[0, 15, 30, 45].map((min) => (
+                    <option key={min} value={min}>
+                      {min.toString().padStart(2, "0")}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="time-row">
+                <label>To:</label>
+                <select
+                  name="toHour"
+                  value={formData.toHour}
+                  onChange={handleChange}
+                  className="dropdown request-time-dropdown"
+                >
+                  <option value="" disabled>Hour</option>
+                  {[...Array(24)].map((_, i) => (
+                    <option key={i} value={i}>
+                      {i.toString().padStart(2, "0")}
+                    </option>
+                  ))}
+                </select>
+
+                <select
+                  name="toMin"
+                  value={formData.toMin}
+                  onChange={handleChange}
+                  className="dropdown request-time-dropdown"
+                >
+                  <option value="" disabled>Min</option>
+                  {[0, 15, 30, 45].map((min) => (
+                    <option key={min} value={min}>
+                      {min.toString().padStart(2, "0")}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <button type="submit" className="submit-button">
+              Request
+            </button>
+          </form>
         </div>
-        <Footer/>
+      </div>
+      <Footer />
     </div>
   );
 };
